ce5a3b9ef8c07fdd1733ba6154f14b879421f358
Kjell Einar Mikkelsen <kem@statoil.com>
2015-09-21 13:42:37 +0200

    Refactored store page routing

--- routes/store.js
+++ routes/store.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET store page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('store', { title: 'Store' });
 });
 
 module.exports = router;
