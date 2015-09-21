fa1a795b77da0c4a0fd9aa45ae9118d4d37c7be5
Kjell Einar Mikkelsen <kem@statoil.com>
2015-09-21 20:28:05 +0200

    added collection count

--- routes/store.js
+++ routes/store.js
@@ -24,8 +24,9 @@ router.route('/message')
 				db.close();
 				res.status(400).send('Error, unable to store message: ' + txtMessage);
 			} else {
+				var docCount = db.collection.count();
 				db.close();
-				console.log('Message stored ok in database: ' + txtMessage)
+				console.log(docCount + ' message stored ok in database: ' + txtMessage)
 				res.status(200).send('Message stored: "' + txtMessage + '"');
 			}
 		});
