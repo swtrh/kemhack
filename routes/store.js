73b32567505c03d653fabc03b730e056c6d24b1c
Kjell Einar Mikkelsen <kem@statoil.com>
2015-09-21 14:19:36 +0200

    Added message func

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('store', { title: 'Store' });
 });
 
+router.route('/message').post(function(req,res, next) {
+	console.log('Receiving message ' + req.body.message);
+	res.send('Message was ' + req.body.message);
+});
+
 module.exports = router;
